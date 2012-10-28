ae20bb58b938831f8755209a2ffc788d300041a0
Tpt <thomaspt@hotmail.fr>
2012-10-28 09:18:31 +0100

    Update of Bootstrap

--- http/templates/header.php
+++ http/templates/header.php
@@ -5,6 +5,8 @@
     <meta charset="UTF-8" />
     <title>WSexport</title>
     <link type="text/css" href="bootstrap.min.css" rel="stylesheet" />
+    <link type="text/css" href="bootstrap-responsive.min.css" rel="stylesheet" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <style type="text/css">
     html, body {
         background-color: #eee;

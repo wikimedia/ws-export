8628a0aa412f9afb4bbee8317f8e417ef86bcc36
Tpt <thomaspt@hotmail.fr>
2015-01-21 18:35:46 +0100

    Adds a beggining of cli script to build OPDS feed
    TODO: make it configurable

---
+++ cli/opds.php
@@ -0,0 +1,29 @@
+#!/usr/bin/php
+<?php
+
+$basePath = realpath(__DIR__ .'/..');
+$tempPath = sys_get_temp_dir();
+
+$wsexportConfig = array(
+	'basePath' => $basePath,
+	'tempPath' => $tempPath,
+	'stat' => true
+);
+include_once($basePath.'/book/init.php');
+
+$lang = 'fr';
+$category = 'Catégorie:Bon_pour_export';
+$outputFile = 'wikisource-fr-good.atom';
+$exportPath = 'http://wsexport.wmflabs.org/tool/book.php';
+
+try {
+	$api = new Api($lang);
+	$provider = new BookProvider($api, array('categories' => false, 'images' => false));
+
+	$atomGenerator = new OpdsBuilder($provider, $lang, $exportPath);
+	$file = file_put_contents($outputFile, $atomGenerator->buildFromCategory($category));
+
+	echo "The OPDS file has been created: $outputFile\n";
+} catch(Exception $exception) {
+	echo "Error: $exception\n";
+}

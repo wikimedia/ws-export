307b1a347c8da82ac663ce976becc25a2a4325d6
Ricordisamoa <ricordisamoa@openmailbox.org>
2015-05-31 23:56:33 +0200

    Use Bootstrap from cdnjs
    Lots of libraries are now mirrored on Tool Labs:
    https://lists.wikimedia.org/pipermail/labs-l/2015-April/003670.html

--- http/templates/header.php
+++ http/templates/header.php
@@ -4,7 +4,7 @@
 <head>
 	<meta charset="UTF-8"/>
 	<title>WSexport</title>
-	<link type="text/css" href="bootstrap.min.css" rel="stylesheet"/>
+	<link type="text/css" href="//tools-static.wmflabs.org/cdnjs/ajax/libs/twitter-bootstrap/3.3.1/css/bootstrap.min.css" rel="stylesheet"/>
 	<meta name="viewport" content="width=device-width, initial-scale=1.0"/>
 	<style type="text/css">
 		html, body {

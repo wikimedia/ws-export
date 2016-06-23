d07d928bc93375cbc28ab94454ebf8be76cc61ab
Jan Berkel <jan@berkel.fr>
2016-06-23 16:04:33 +0200

    Explicitly load test dependencies

--- tests/utils/ApiTest.php
+++ tests/utils/ApiTest.php
@@ -1,5 +1,7 @@
 <?php
 
+require_once __DIR__ . '/../test_init.php';
+
 use GuzzleHttp\Client;
 use GuzzleHttp\Handler\MockHandler;
 use GuzzleHttp\HandlerStack;
--- tests/utils/LoggingMiddlewareTest.php
+++ tests/utils/LoggingMiddlewareTest.php
@@ -1,5 +1,7 @@
 <?php
 
+require_once __DIR__ . '/../test_init.php';
+
 use GuzzleHttp\HandlerStack;
 use GuzzleHttp\Handler\MockHandler;
 use GuzzleHttp\Psr7\Request;

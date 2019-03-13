a1688e9674a3f87dd5cb1983b99291aaa8b3bcd9
Tpt <thomaspt@hotmail.fr>
2019-03-13 20:49:00 +0100

    Upgrades PHP minimal version to 7.2

--- tests/utils/UtilsTest.php
+++ tests/utils/UtilsTest.php
@@ -7,6 +7,6 @@ class UtilsTest extends \PHPUnit\Framework\TestCase {
 	 * @covers ::encodeString
 	 */
 	public function testEncodeString() {
-		 $this->assertEquals( 'c0_test_dou', encodeString( 'test_Δôü' ) );
+		 $this->assertStringEndsWith( '_test_dou', encodeString( 'test_Δôü' ) );
 	}
 }

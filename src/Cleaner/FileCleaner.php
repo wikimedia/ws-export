2fcf826411ff97b60fa4bf78e91092d046800302
Tpt <thomaspt@hotmail.fr>
2020-04-25 14:38:07 +0200

    Tries to fix problems with grayscale jpeg and Adobe Reader

--- src/Cleaner/FileCleaner.php
+++ src/Cleaner/FileCleaner.php
@@ -3,6 +3,8 @@
 namespace App\Cleaner;
 
 use Imagick;
+use ImagickDraw;
+use ImagickPixel;
 
 class FileCleaner {
 	public static function cleanFile( string $fileName, string $mimetype ) {
@@ -13,10 +15,19 @@ class FileCleaner {
 
 	private static function cleanJPG( string $fileName ) {
 		$imagic = new Imagick( $fileName );
-		if ( $imagic->getImageDepth() === 8 ) {
-			// Some PDF readers do not like 8bits images
-			$imagic->setImageDepth( 32 );
+		if ( $imagic->getImageColorspace() === Imagick::COLORSPACE_GRAY ) {
+			// Some PDF readers do not like grayscale images
+			$imagic->setImageColorspace( Imagick::COLORSPACE_SRGB );
+
+			// Hack to add a not greyscale color to the image to make Imagic to save it as sRGB
+			$draw = new ImagickDraw();
+			$draw->setFillColor( new ImagickPixel( "red" ) );
+			$draw->setfillopacity( 0.0001 );
+			$draw->point( 0, 0 );
+			$imagic->drawImage( $draw );
+
+			$imagic->writeImage( $fileName );
 		}
-		$imagic->writeImage( $fileName );
+		$imagic->clear();
 	}
 }

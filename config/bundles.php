369b84905ac25d08959b204e97c232b3d2a9f644
Sam Wilson <sam@samwilson.id.au>
2020-10-20 08:44:26 +0800

    Add logging, including emailing fatals in prod
    Add Monolog and Symfony Mailer, the latter for emailing fatal
    errors from production to the tool's maintainers' email addresses.

--- config/bundles.php
+++ config/bundles.php
@@ -8,4 +8,5 @@ return [
 	Doctrine\Bundle\DoctrineBundle\DoctrineBundle::class => [ 'all' => true ],
 	Wikimedia\ToolforgeBundle\ToolforgeBundle::class => [ 'all' => true ],
 	Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => [ 'dev' => true, 'test' => true ],
+	Symfony\Bundle\MonologBundle\MonologBundle::class => [ 'all' => true ],
 ];

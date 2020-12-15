64e37c203776055f1ec56e0f83aaedaf69dab9d8
Dayllan Maza <dmaza@wikimedia.org>
2020-12-15 13:29:32 -0500

    more

--- src/Command/ConsumerCommand.php
+++ src/Command/ConsumerCommand.php
@@ -3,43 +3,77 @@
 namespace App\Command;
 
 use Symfony\Component\Console\Command\Command;
+use Symfony\Contracts\Cache\CacheInterface;
+use Symfony\Contracts\Cache\ItemInterface;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Style\SymfonyStyle;
 use PhpAmqpLib\Connection\AMQPStreamConnection;
+use PhpAmqpLib\Message\AMQPMessage;
+use Symfony\Component\Cache\Adapter\FilesystemAdapter;
 
 class ConsumerCommand extends Command {
 
     protected static $defaultName = 'app:consume';
 
+    private $cache;
+
+    /** @var SymfonyStyle */
+    private $io;
+
     public function __construct()
     {
         parent::__construct();
+        $this->cache = new FilesystemAdapter();
     }
 
     protected function execute(InputInterface $input, OutputInterface $output)
     {
+        $this->io = new SymfonyStyle($input, $output);
+
         $connection = new AMQPStreamConnection('rabbitmq', 5672, 'admin', 'pass');
         $channel = $connection->channel();
 
-        $channel->queue_declare('exports_queue', false, true, false, false);
+        $channel->queue_declare('exports_queue', false, false, false, false);
 
-        $io = new SymfonyStyle($input, $output);
-        $io->success("[*] Waiting for messages. To exit press CTRL+C\n");
-
-        $callback = function ($msg) {
-            echo ' [x] Received ', $msg->body, "\n";
-            $msg->delivery_info['channel']->basic_ack($msg->delivery_info['delivery_tag']);
-        };
+        $this->io->success("[*] Waiting for messages. To exit press CTRL+C");
 
         // TODO: look into this
         // $channel->basic_qos(null, 3, null);
-        $channel->basic_consume('exports_queue', '', false, false, false, false, $callback);
+        $channel->basic_consume(
+            'exports_queue', '', false, false, false, false, [$this, 'processMessageCallback']);
 
         while ($channel->is_consuming()) {
             $channel->wait();
         }
 
+        $channel->close();
+        $connection->close();
+
         return Command::SUCCESS;
+    }
+
+    public function processMessageCallback( AMQPMessage $r ) {
+        echo ' [x] Received request to export ', $r->body, "\n";
+
+        $jobInProcess = $this->cache->getItem( $r->get('correlation_id'));
+        if ($jobInProcess->isHit()) {
+            $this->io->error("[*] Book export in process");
+            $r->getChannel()->basic_nack($r->getDeliveryTag());
+
+            return;
+        }
+        $jobInProcess->expiresAfter(30); // for the purpose of testing
+        $this->cache->save($jobInProcess);
+
+        $msg = new AMQPMessage( $r->body, [ 'correlation_id' => $r->get('correlation_id')]);
+
+        sleep(5);
+
+        $r->getChannel()->exchange_declare('exports_done', 'fanout', false, false, false);
+        $r->getChannel()->basic_publish( $msg, 'exports_done', $r->get('reply_to'));
+        $r->getChannel()->basic_ack($r->getDeliveryTag());
+
+        $this->cache->delete($r->get('correlation_id'));
     }
 }
--- src/Command/ProducerCommand.php
+++ src/Command/ProducerCommand.php
@@ -4,6 +4,7 @@ namespace App\Command;
 
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Style\SymfonyStyle;
 use PhpAmqpLib\Connection\AMQPStreamConnection;
@@ -18,26 +19,72 @@ class ProducerCommand extends Command {
         parent::__construct();
     }
 
+    protected function configure() {
+		$this->setDescription( 'Export a book.' )
+			->addOption( 'title', 't', InputOption::VALUE_REQUIRED, 'Wiki page name of the work to export. Required' );
+	}
+
     protected function execute(InputInterface $input, OutputInterface $output)
     {
+        $io = new SymfonyStyle( $input, $output );
+
+		if ( !$input->getOption( 'title' ) ) {
+			$io->warning( 'Please provide a title with the --title option.' );
+			return Command::FAILURE;
+        }
+
+        $response = null;
+        $corrId = md5($input->getOption('title'));
+
         $connection = new AMQPStreamConnection('rabbitmq', 5672, 'admin', 'pass');
         $channel = $connection->channel();
 
-        $channel->queue_declare('exports_queue', false, true, false, false);
+        // set callback channel and exchange
+        list($callback_queue, ,) = $channel->queue_declare("", false, false, true, false);
+        $channel->exchange_declare('exports_done', 'fanout', false, false, false);
+        $channel->queue_bind($callback_queue, 'exports_done');
 
-        $data = "Some data to export a book";
+        $channel->basic_consume($callback_queue, '', false, false, false, false, function( $r ) use ( &$response, $corrId ) {
+            if ($r->get('correlation_id') == $corrId ) {
+                $response = $r->body;
+            }
+        });
+
+        // set publish channel
+        $channel->queue_declare('exports_queue', false, false, false, false);
+
+        $data = $input->getOption( 'title' );
         $msg = new AMQPMessage(
             $data,
-            array('delivery_mode' => AMQPMessage::DELIVERY_MODE_PERSISTENT)
+            array(
+                'correlation_id' => $corrId,
+                'reply_to' => $callback_queue,
+           )
         );
 
         $channel->basic_publish($msg, '', 'exports_queue');
 
+        $io->success(" [x] Sent request to export " . $input->getOption('title') );
+
+        try {
+            while (!$response) {
+                $channel->wait(null, false, 10);
+            }
+            // TODO: catch proper exception for timeones
+        } catch ( \Exception $e) {
+            $io->error(" [x] We are working on your book and it might take a while. Come back here http://somenewurl in a few minutes for an update" );
+
+            $channel->close();
+            $connection->close();
+
+            return Command::FAILURE;
+            
+        }
+
+        $io->success(" [x] ". $response . " was successfully exported" );
+
         $channel->close();
         $connection->close();
-
-        $io = new SymfonyStyle( $input, $output );
-        $io->success(" [x] Sent \n");
 
         return Command::SUCCESS;
     }

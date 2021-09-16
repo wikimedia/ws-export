fbe577567d77bea34b89610e3aedd04e9caef0bd
Tpt <thomaspt@hotmail.fr>
2021-09-16 21:33:56 +0200

    Drops the HTML file splitting hack
    Useful for old reading software like Adobe versions before 2009.
    
    Should be ok now hopefully with moderns reading systems.
    
    Bug: T261238

--- src/Cleaner/BookCleanerEpub.php
+++ src/Cleaner/BookCleanerEpub.php
@@ -3,7 +3,6 @@
 namespace App\Cleaner;
 
 use App\Book;
-use App\Page;
 use App\PageParser;
 use App\Util\Util;
 use DOMDocument;
@@ -27,7 +26,6 @@ class BookCleanerEpub {
 		$this->baseUrl = $baseUrl;
 
 		$this->encodeTitles();
-		$this->splitChapters();
 
 		if ( $book->content ) {
 			$xPath = $this->getXPath( $book->content );
@@ -44,124 +42,6 @@ class BookCleanerEpub {
 				$this->cleanHtml( $xPath );
 			}
 		}
-	}
-
-	protected function splitChapters() {
-		$chapters = [];
-		if ( $this->book->content ) {
-			$main = $this->splitChapter( $this->book );
-			$this->book->content = $main[0]->content;
-			if ( !empty( $main ) ) {
-				unset( $main[0] );
-				$chapters = $main;
-			}
-		}
-		foreach ( $this->book->chapters as $chapter ) {
-			$chapters = array_merge( $chapters, $this->splitChapter( $chapter ) );
-		}
-		$this->book->chapters = $chapters;
-	}
-
-	/*
-	 * Credit for the tricky part of this code: Asbjorn Grandt
-	 * https://github.com/Grandt/PHPePub/blob/master/EPubChapterSplitter.php
-	 */
-	protected function splitChapter( Page $chapter ) {
-		$partSize = 250000;
-		$length = strlen( $chapter->content->saveXML() );
-		if ( $length <= $partSize ) {
-			return [ $chapter ];
-		}
-
-		$parts = ceil( $length / $partSize );
-		$partSize = ( $length / $parts ) + 2000;
-
-		$pages = [];
-
-		$files = [];
-		$domDepth = 0;
-		$domPath = [];
-		$domClonedPath = [];
-
-		$curFile = $chapter->content->createDocumentFragment();
-		$files[] = $curFile;
-		$curParent = $curFile;
-		$curSize = 0;
-
-		$head = $chapter->content->getElementsByTagName( 'head' )->item( 0 );
-		$body = $chapter->content->getElementsByTagName( 'body' )->item( 0 );
-		$node = $body->firstChild;
-		do {
-			$nodeData = $chapter->content->saveXML( $node );
-			$nodeLen = strlen( $nodeData );
-
-			if ( $nodeLen > $partSize && $node->hasChildNodes() ) {
-				$domPath[] = $node;
-				$domClonedPath[] = $node->cloneNode( false );
-				$domDepth++;
-
-				$node = $node->firstChild;
-
-				$nodeData = $chapter->content->saveXML( $node );
-				$nodeLen = strlen( $nodeData );
-			}
-
-			$next_node = $node->nextSibling;
-
-			if ( $node != null && $node->nodeName !== "#text" ) {
-				if ( $curSize > 0 && $curSize + $nodeLen > $partSize ) {
-					$curFile = $chapter->content->createDocumentFragment();
-					$files[] = $curFile;
-					$curParent = $curFile;
-					if ( $domDepth > 0 ) {
-						foreach ( $domClonedPath as $v ) {
-							$newParent = $v->cloneNode( false );
-							$curParent->appendChild( $newParent );
-							$curParent = $newParent;
-						}
-					}
-					$curSize = strlen( $chapter->content->saveXML( $curFile ) );
-				}
-			}
-
-			$curParent->appendChild( $node->cloneNode( true ) );
-			$curSize += $nodeLen;
-
-			$node = $next_node;
-			while ( $node == null && $domDepth > 0 ) {
-				$domDepth--;
-				$node = end( $domPath )->nextSibling;
-				array_pop( $domPath );
-				array_pop( $domClonedPath );
-				if ( $curParent->parentNode ) {
-					$curParent = $curParent->parentNode;
-				}
-			}
-		} while ( $node != null );
-
-		foreach ( $files as $idx => $file ) {
-			$xml = $this->getEmptyDom();
-			$newHead = $xml->getElementsByTagName( 'head' )->item( 0 );
-			foreach ( $head->childNodes as $childNode ) {
-				$newHead->appendChild( $xml->importNode( $childNode, true ) );
-			}
-			$newBody = $xml->getElementsByTagName( 'body' )->item( 0 );
-			$newBody->appendChild( $xml->importNode( $file, true ) );
-			foreach ( $body->attributes as $attribute ) {
-				$newBody->setAttribute( $attribute->nodeName, $attribute->nodeValue );
-			}
-			$page = new Page();
-			if ( $idx == 0 ) {
-				$page->title = $chapter->title;
-				$page->name = $chapter->name;
-			} else {
-				$page->title = $chapter->title . '_' . ( $idx + 1 );
-			}
-			$page->content = $xml;
-			$pages[] = $page;
-		}
-
-		return $pages;
 	}
 
 	protected function encodeTitles() {

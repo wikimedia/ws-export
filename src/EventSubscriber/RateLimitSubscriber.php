c91c6947921e797771931a4c4a1a594a7a305b91
Sam Wilson <sam@samwilson.id.au>
2021-11-04 13:27:50 +0800

    Add logging for rate-limiter, and fix small CSS bug
    * Add some debugging for the rate-limiting, to help figure out what's going on.
    * Fix the display of the error message icon (noticed because the rate-limiting
      message is more than one line).
    
    Bug: T286048

--- src/EventSubscriber/RateLimitSubscriber.php
+++ src/EventSubscriber/RateLimitSubscriber.php
@@ -6,6 +6,7 @@ namespace App\EventSubscriber;
 use DateInterval;
 use Krinkle\Intuition\Intuition;
 use Psr\Cache\CacheItemPoolInterface;
+use Psr\Log\LoggerInterface;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 use Symfony\Component\HttpKernel\Event\ControllerEvent;
 use Symfony\Component\HttpKernel\Exception\TooManyRequestsHttpException;
@@ -25,6 +26,9 @@ class RateLimitSubscriber implements EventSubscriberInterface {
 	/** @var int */
 	protected $rateDuration;
 
+	/** @var LoggerInterface */
+	protected $logger;
+
 	/**
 	 * @param Intuition $intuition
 	 * @param CacheItemPoolInterface $cache
@@ -35,12 +39,14 @@ class RateLimitSubscriber implements EventSubscriberInterface {
 		Intuition $intuition,
 		CacheItemPoolInterface $cache,
 		int $rateLimit,
-		int $rateDuration
+		int $rateDuration,
+		LoggerInterface $logger
 	) {
 		$this->intuition = $intuition;
 		$this->cache = $cache;
 		$this->rateLimit = $rateLimit;
 		$this->rateDuration = $rateDuration;
+		$this->logger = $logger;
 	}
 
 	/**
@@ -70,30 +76,45 @@ class RateLimitSubscriber implements EventSubscriberInterface {
 
 		// Abort if rate limitations are disabled or we're not exporting a book.
 		if ( $this->rateLimit + $this->rateDuration === 0 || $action !== 'home' || !$request->get( 'page' ) ) {
+			$this->log( 'Not enabled for this request.' );
 			return;
 		}
 
 		$xff = $request->headers->get( 'x-forwarded-for', '' );
 		if ( $xff === '' ) {
 			// Happens in local environments, or outside of Cloud Services.
+			$this->log( 'No X-Forwarded-For header value found.' );
 			return;
 		}
 
 		$cacheKey = "ratelimit.session." . md5( $xff );
+		$this->log( 'cache key: ' . $cacheKey );
 		$cacheItem = $this->cache->getItem( $cacheKey );
+		$this->log( 'Cache ' . ( $cacheItem->isHit() ? 'hit' : 'miss' ) );
 
 		// If increment value already in cache, or start with 1.
 		$count = $cacheItem->isHit() ? (int)$cacheItem->get() + 1 : 1;
+		$this->log( 'Count: ' . $count );
 
 		// Check if limit has been exceeded, and if so, throw an error.
 		if ( $count > $this->rateLimit ) {
+			$this->log( 'Rate limited.' );
 			$this->denyAccess();
 		}
 
 		// Reset the clock on every request.
+		$this->log( 'Saving new count: ' . $count );
 		$cacheItem->set( $count )
 			->expiresAfter( new DateInterval( 'PT' . $this->rateDuration . 'M' ) );
 		$this->cache->save( $cacheItem );
+	}
+
+	/**
+	 * Log a debug message.
+	 * @param string $msg The log message.
+	 */
+	private function log( string $msg ) {
+		$this->logger->debug( "[rate-limiter] $msg" );
 	}
 
 	/**
